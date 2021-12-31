13c841aa635f2048e4339f9a4d5caedf03b3c4cd
Jakub Zmuda <65454612+KubaZm@users.noreply.github.com>
2021-12-31 19:30:39 +0100

    JPA implementation
    Implemented JPA for ptm_drivers and ptm_users

--- api/src/main/java/pl/publictransportmanager/publictransportmanagerapi/repositories/DriverRepository.java
+++ api/src/main/java/pl/publictransportmanager/publictransportmanagerapi/repositories/DriverRepository.java
@@ -1,21 +1,10 @@
 package pl.publictransportmanager.publictransportmanagerapi.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.publictransportmanager.publictransportmanagerapi.domain.Driver;
-import pl.publictransportmanager.publictransportmanagerapi.exceptions.PtmBadRequestException;
-import pl.publictransportmanager.publictransportmanagerapi.exceptions.PtmResourceNotFoundException;
 
-import java.util.List;
+@Repository
+public interface DriverRepository extends JpaRepository<Driver, Integer> {
 
-public interface DriverRepository {
-
-    List<Driver> findAll() throws PtmResourceNotFoundException;
-
-    Driver findById(Integer driverId) throws PtmResourceNotFoundException;
-
-    Integer create(String pesel, String name, String surname, String phone_number,
-                   String email, String address, Double salary) throws PtmBadRequestException;
-
-    void update(Integer driverId, Driver driver) throws PtmBadRequestException;
-
-    void removeById(Integer driverId);
 }
