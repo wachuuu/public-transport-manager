52c0b1769d3378117e4fe4750d1fbe115d74299a
Jakub Zmuda <65454612+KubaZm@users.noreply.github.com>
2022-01-07 21:23:19 +0100

    Token validity extended

--- api/src/main/java/pl/publictransportmanager/publictransportmanagerapi/Constants.java
+++ api/src/main/java/pl/publictransportmanager/publictransportmanagerapi/Constants.java
@@ -3,5 +3,5 @@ package pl.publictransportmanager.publictransportmanagerapi;
 public class Constants {
     public static final String API_SECRET_KEY = "publictransportmanagerapi";
 
-    public static final long TOKEN_VALIDITY = 2 * 60 * 60 * 1000;
+    public static final long TOKEN_VALIDITY = 24 * 60 * 60 * 1000;
 }
